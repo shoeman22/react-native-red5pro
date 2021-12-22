677d7abb30209c9355e11c21e63733413d51145d
Andy Schuler <shoeman22@users.noreply.github.com>
2021-12-22 13:30:29 -0700

    Update PublishService.java

--- android/src/main/java/com/red5pro/reactnative/view/PublishService.java
+++ android/src/main/java/com/red5pro/reactnative/view/PublishService.java
@@ -77,8 +77,8 @@ public class PublishService extends Service {
 				}
 
 				holderNote = notificationBuilder
-						.setContentTitle("Kwivrr")
-						.setContentText("Continuing to stream from background.")
+						.setContentTitle("Live Streaming")
+						.setContentText("Continuing to broadcast from background")
 						.setSmallIcon(android.R.drawable.ic_media_play)
 						.build();
 				startForeground(57234111, holderNote);
